f744e01b0b0822087b2a05c4d34106ebc30f9530
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-07-21 21:00:45 +0100

    deploy fixes

--- src/components/cards/PreviewCard.vue
+++ src/components/cards/PreviewCard.vue
@@ -20,7 +20,7 @@
     <div class="w-full bg-white rounded-2xl input-border h-[313px] flex relative">
       <div class="m-auto">
         <img
-          src="/../../transparent-baby.png"
+          src="https://polyte-app.herokuapp.com/transparent-baby.png"
           class="w-[300px] h-full"
           alt=""
         />
